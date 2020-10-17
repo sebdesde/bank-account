38d79dccf22f4ae9759a8a5f4c19ac112f805a6b
Sébastien Robalo <sebastien.robalo@daveo.fr>
2020-10-17 13:34:12 +0200

    Ajout d'un test de dépôt d'argent avec centimes

--- BankAccount/BankAccountUnitTests/DepositAccountTests.cs
+++ BankAccount/BankAccountUnitTests/DepositAccountTests.cs
@@ -12,5 +12,13 @@ namespace BankAccountUnitTests
             account.Amount += 5;
             Assert.AreEqual(5, account.Amount);
         }
+
+        [Test]
+        public void Deposit_5_42_euros_on_a_new_account()
+        {
+            Account account = new Account();
+            account.Amount += 5.42m;
+            Assert.AreEqual(5.42m, account.Amount);
+        }
     }
 }
