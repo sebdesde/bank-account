4909adda300b44161eef8b7f4e689a0b9550e849
Sébastien Robalo <sebastien.robalo@daveo.fr>
2020-10-17 13:07:52 +0200

    Ajout du 1er test unitaire concernant le dépot d'argent sur un nouveau compte

---
+++ BankAccount/BankAccount/Models/Account.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankAccount.Models
+{
+    public class Account
+    {
+        public decimal Amount { get; set; }
+    }
+}
--- BankAccount/BankAccountUnitTests/DepositAccountTests.cs
+++ BankAccount/BankAccountUnitTests/DepositAccountTests.cs
@@ -1,10 +1,15 @@
+using BankAccount.Models;
 using NUnit.Framework;
 
 namespace BankAccountUnitTests
 {
     public class DepositAccountTests
     {
-
-
+        public void Deposit_5_euros_on_a_new_account()
+        {
+            Account account = new Account();
+            account.Amount += 5;
+            Assert.Equals(5, account.Amount);
+        }
     }
 }
